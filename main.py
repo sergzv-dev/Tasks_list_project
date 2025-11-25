a505a074f14ac168d3b0c5e0908847827dde245e
sergzv-dev <serg.zv.dev@gmail.com>
2025-11-26 00:19:47 +0400

    bug fix

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
 from repository import UserRepository, TaskRepository
-from models import AddUserModel, UserModel, AddTaskModel, TaskModel, UserTaskModel, ChangeTaskModel
-from typing import List
+from models import AddUserModel, AddTaskModel, ChangeTaskModel
 
 app = FastAPI()
 
 task_repo = TaskRepository('test.db')
 user_repo = UserRepository('test.db')
-
-def return_tm_list(task_list: list):
-    return [TaskModel(task_id = t[0], task = t[1], done = t[2], user_id = t[3]) for t in task_list]
 
 
 @app.post('/users/new_user')
@@ -17,10 +13,9 @@ def add_user(user: AddUserModel) -> dict:
     user_repo.add(user)
     return {'message': f'user added'}
 
-@app.get('/users/all_users', response_model = List[UserModel])
+@app.get('/users/all_users')
 def all_users():
-    db_users = user_repo.all_users()
-    return [UserModel(user_id = user[0], user_name = user[1]) for user in db_users]
+    return user_repo.all_users()
 
 @app.post('/users/{user_id}/dell_user')
 def dell_user(user_id: int) -> dict:
@@ -28,37 +23,33 @@ def dell_user(user_id: int) -> dict:
     return {'message': f'user deleted'}
 
 @app.post('/tasks/new_task')
-def add_task(new_task: AddTaskModel):
+def add_task(new_task: AddTaskModel) -> dict:
     task_repo.add(new_task)
     return {'message': 'task added'}
 
 @app.post('/tasks/{task_id}/finish_task')
-def finish_task(task_id: int):
+def finish_task(task_id: int) -> dict:
     task_repo.finish_task(task_id)
     return {'message': f'task complete'}
 
 @app.get("/tasks/all_tasks")
 def all_tasks():
-    db_tasks = task_repo.all_tasks()
-    return return_tm_list(db_tasks)
+    return task_repo.all_tasks()
 
 @app.get("/tasks/done_tasks")
 def done_tasks():
-    db_tasks = task_repo.get_tasks_by_status(True)
-    return return_tm_list(db_tasks)
+    return task_repo.get_tasks_by_status(True)
 
 @app.get("/tasks/not_done_tasks")
 def not_done_tasks():
-    db_tasks = task_repo.get_tasks_by_status(False)
-    return return_tm_list(db_tasks)
+    return task_repo.get_tasks_by_status(False)
 
-@app.get("/tasks{user_id}/user_tasks")
+@app.get("/tasks/{user_id}/user_tasks")
 def user_tasks(user_id: int):
-    db_us_ts = task_repo.user_tasks(user_id)
-    return [UserTaskModel(task_id=ut[0], task=ut[1], done=ut[2], user_id=ut[3], user_name=ut[4]) for ut in db_us_ts]
+    return task_repo.user_tasks(user_id)
 
 @app.post('/tasks/change_task')
-def change_task(new_task: ChangeTaskModel):
+def change_task(new_task: ChangeTaskModel) -> dict:
     task_repo.change_task(new_task)
     return {'message': f'task changed'}
 
--- repository.py
+++ repository.py
@@ -1,5 +1,5 @@
 import sqlite3
-from models import AddUserModel, AddTaskModel, ChangeTaskModel
+from models import AddUserModel, UserModel, AddTaskModel, TaskModel, UserTaskModel, ChangeTaskModel
 
 class Repository:
     def __init__(self, db_path = 'test.db'):
@@ -17,7 +17,8 @@ class UserRepository(Repository):
         with sqlite3.connect(self.db_path) as conn:
             cursor = conn.cursor()
             cursor.execute('SELECT * FROM users')
-            return cursor.fetchall()
+            db_users = cursor.fetchall()
+            return [UserModel(user_id=user[0], user_name=user[1]) for user in db_users]
 
     def dell_user(self, user_id: int):
         with sqlite3.connect(self.db_path) as conn:
@@ -46,7 +47,8 @@ class TaskRepository(Repository):
             cursor.execute('''SELECT t.task_id, t.task, t.done, u.user_id, u.user_name
                             FROM tasks AS t
                             INNER JOIN users AS u ON t.user_id = u.user_id''')
-            return cursor.fetchall()
+            task_list = cursor.fetchall()
+            return [TaskModel(task_id = t[0], task = t[1], done = t[2], user_id = t[3]) for t in task_list]
 
     def get_tasks_by_status(self, status: bool):
         with sqlite3.connect(self.db_path) as conn:
@@ -55,7 +57,9 @@ class TaskRepository(Repository):
                             FROM tasks AS t
                             INNER JOIN users AS u ON t.user_id = u.user_id
                             WHERE t.done = :status''', {'status': status})
-            return cursor.fetchall()
+            task_list = cursor.fetchall()
+            return [TaskModel(task_id=t[0], task=t[1], done=t[2], user_id=t[3]) for t in task_list]
+
 
     def user_tasks(self, user_id: int):
         with sqlite3.connect(self.db_path) as conn:
@@ -64,7 +68,8 @@ class TaskRepository(Repository):
                             FROM tasks AS t
                             INNER JOIN users AS u ON t.user_id = u.user_id
                             WHERE u.user_id = :user_id''', {'user_id': user_id })
-            return cursor.fetchall()
+            db_us_ts = cursor.fetchall()
+            return [UserTaskModel(task_id=ut[0], task=ut[1], done=ut[2], user_id=ut[3], user_name=ut[4]) for ut in db_us_ts]
 
     def change_task(self, new_task: ChangeTaskModel):
         with sqlite3.connect(self.db_path) as conn:
